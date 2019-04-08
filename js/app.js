48588530f9510446e7ab3f40b0740d1643329a86
Jhia Turner <jhimitu@gmail.com>
2019-04-08 15:39:04 -0700

    diplay random photos on page

--- js/app.js
+++ js/app.js
@@ -2,34 +2,35 @@
 
 let images = [];
 
-function ProductImage(imgName) {
+function ProductImage(imgName, filetype) {
   this.name = imgName;
-  this.filepath = `img/${imgName}.jpg`;
+  this.filetype = filetype;
+  this.filepath = `img/${imgName}.${filetype}`;
   this.clicks = 0;
   this.views = 0;
   images.push(this);
 }
 
-new ProductImage('bag');
-new ProductImage('banana');
-new ProductImage('bathroom');
-new ProductImage('boots');
-new ProductImage('breakfast');
-new ProductImage('bubblegum');
-new ProductImage('chair');
-new ProductImage('cthulhu');
-new ProductImage('dog-duck');
-new ProductImage('dragon');
-new ProductImage('pen');
-new ProductImage('pet-sweep');
-new ProductImage('scissors');
-new ProductImage('shark');
-new ProductImage('sweep');
-new ProductImage('tauntaun');
-new ProductImage('unicorn');
-new ProductImage('usb');
-new ProductImage('water-can');
-new ProductImage('wine-glass');
+new ProductImage('bag', 'jpg');
+new ProductImage('banana', 'jpg');
+new ProductImage('bathroom', 'jpg');
+new ProductImage('boots', 'jpg');
+new ProductImage('breakfast', 'jpg');
+new ProductImage('bubblegum', 'jpg');
+new ProductImage('chair', 'jpg');
+new ProductImage('cthulhu', 'jpg');
+new ProductImage('dog-duck', 'jpg');
+new ProductImage('dragon', 'jpg');
+new ProductImage('pen', 'jpg');
+new ProductImage('pet-sweep', 'jpg');
+new ProductImage('scissors', 'jpg');
+new ProductImage('shark', 'jpg');
+new ProductImage('sweep', 'png');
+new ProductImage('tauntaun', 'jpg');
+new ProductImage('unicorn', 'jpg');
+new ProductImage('usb', 'gif');
+new ProductImage('water-can', 'jpg');
+new ProductImage('wine-glass', 'jpg');
 
 function generateRandomImages() {
   let threeRandomImages = [];
@@ -38,6 +39,14 @@ function generateRandomImages() {
     threeRandomImages.push(images[Math.floor(Math.random() * images.length)].filepath);
   }
   console.log(threeRandomImages);
+
+  let display = document.getElementById('products');
+
+  threeRandomImages.forEach((image) => {
+    let product = document.createElement('IMG');
+    product.setAttribute('src', `${image}`);
+    display.appendChild(product);
+  });
 }
 
 generateRandomImages();
